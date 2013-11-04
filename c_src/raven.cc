36473d22818dd432309d2860e4b77392d5fc7d51
Erik Hedenström <erik@hedenstroem.com>
2013-11-04 23:47:19 +0100

    Started implemented blueprints

--- c_src/raven.cc
+++ c_src/raven.cc
@@ -38,8 +38,13 @@ static ERL_NIF_TERM a_not_implemented;
 static ERL_NIF_TERM a_result;
 static ERL_NIF_TERM a_blueprint;
 static ERL_NIF_TERM a_source_annotation;
+static ERL_NIF_TERM a_resource_group;
+static ERL_NIF_TERM a_resource;
+static ERL_NIF_TERM a_payload;
 
-static void init_atoms(ErlNifEnv* env) {
+static ERL_NIF_TERM l_empty;
+
+static void init_terms(ErlNifEnv* env) {
 
     a_json = enif_make_atom(env, "json");
     a_yaml = enif_make_atom(env, "yaml");
@@ -51,22 +56,31 @@ static void init_atoms(ErlNifEnv* env) {
     a_result = enif_make_atom(env, "result");
     a_blueprint = enif_make_atom(env, "blueprint");
     a_source_annotation = enif_make_atom(env, "source_annotation");
+    a_resource_group = enif_make_atom(env, "resource_group");
+    a_resource = enif_make_atom(env, "resource");
+    a_payload = enif_make_atom(env, "payload");
+
+    l_empty = enif_make_list(env, 0);
 
 }
 
 static int on_load(ErlNifEnv* env, void**, ERL_NIF_TERM) {
-    init_atoms(env);
+    init_terms(env);
     return 0;
 }
 
-static ERL_NIF_TERM wrap_error(ErlNifEnv* env, Error error) {
-    return a_not_implemented;
+static ERL_NIF_TERM wrap_string(ErlNifEnv* env, const std::string str) {
+    if (str.empty()) {
+        return l_empty;
+    } else {
+        return enif_make_string(env, str.c_str(), ERL_NIF_LATIN1);
+    }
 }
 
-static ERL_NIF_TERM wrap_annotation(ErlNifEnv* env, const SourceAnnotation& annotation, const char *dict[]) {
+static ERL_NIF_TERM wrap_annotation(ErlNifEnv* env, const SourceAnnotation annotation, const char *dict[]) {
     ERL_NIF_TERM code = enif_make_int(env, annotation.code);
     ERL_NIF_TERM type = enif_make_atom(env, dict[annotation.code]);
-    ERL_NIF_TERM message = enif_make_string(env, annotation.message.empty() ? "" : annotation.message.c_str(), ERL_NIF_LATIN1);
+    ERL_NIF_TERM message = wrap_string(env, annotation.message);
     ERL_NIF_TERM list = enif_make_list(env, 0);
     if (!annotation.location.empty()) {
         for (SourceCharactersBlock::const_iterator it = annotation.location.begin();it != annotation.location.end();++it) {
@@ -79,7 +93,7 @@ static ERL_NIF_TERM wrap_annotation(ErlNifEnv* env, const SourceAnnotation& anno
     return enif_make_tuple5(env, a_source_annotation, code, type, message, list);
 }
 
-static ERL_NIF_TERM wrap_warnings(ErlNifEnv* env, Warnings warnings) {
+static ERL_NIF_TERM wrap_warnings(ErlNifEnv* env, const Warnings warnings) {
     ERL_NIF_TERM list = enif_make_list(env, 0);
     for (Warnings::const_iterator it = warnings.begin(); it != warnings.end(); ++it) {
         list = enif_make_list_cell(env,wrap_annotation(env,*it,warning_str),list);
@@ -87,12 +101,91 @@ static ERL_NIF_TERM wrap_warnings(ErlNifEnv* env, Warnings warnings) {
     return list;
 }
 
-static ERL_NIF_TERM wrap_result(ErlNifEnv* env, Result result) {
+static ERL_NIF_TERM wrap_result(ErlNifEnv* env, const Result result) {
     return enif_make_tuple3(env, a_result, wrap_annotation(env,result.error,error_str), wrap_warnings(env,result.warnings));
 }
 
-static ERL_NIF_TERM wrap_blueprint(ErlNifEnv* env, Blueprint blueprint) {
+static ERL_NIF_TERM wrap_kvpairs(ErlNifEnv* env, const Collection<KeyValuePair>::type kvpairs) {
+    ERL_NIF_TERM list = enif_make_list(env, 0);
+    for (Collection<KeyValuePair>::const_iterator it = kvpairs.begin(); it != kvpairs.end(); ++it) {
+        ERL_NIF_TERM key = wrap_string(env, it->first);
+        ERL_NIF_TERM value = wrap_string(env, it->second);
+        list = enif_make_list_cell(env,enif_make_tuple2(env,key,value),list);
+    }
+    return list;
+}
+
+static ERL_NIF_TERM wrap_parameter(ErlNifEnv* env, const Parameter parameter) {
     return a_not_implemented;
+}
+
+static ERL_NIF_TERM wrap_parameters(ErlNifEnv* env, const Collection<Parameter>::type parameters) {
+    ERL_NIF_TERM list = enif_make_list(env, 0);
+    for (Collection<Parameter>::const_iterator it = parameters.begin(); it != parameters.end(); ++it) {
+        list = enif_make_list_cell(env,wrap_parameter(env,*it),list);
+    }
+    return list;
+}
+
+static ERL_NIF_TERM wrap_action(ErlNifEnv* env, const Action action) {
+    return a_not_implemented;
+}
+
+static ERL_NIF_TERM wrap_actions(ErlNifEnv* env, const Collection<Action>::type actions) {
+    ERL_NIF_TERM list = enif_make_list(env, 0);
+    for (Collection<Action>::const_iterator it = actions.begin(); it != actions.end(); ++it) {
+        list = enif_make_list_cell(env,wrap_action(env,*it),list);
+    }
+    return list;
+}
+
+static ERL_NIF_TERM wrap_payload(ErlNifEnv* env, const Payload payload) {
+    ERL_NIF_TERM name = wrap_string(env, payload.name);
+    ERL_NIF_TERM description = wrap_string(env, payload.description);
+    ERL_NIF_TERM parameters = wrap_parameters(env, payload.parameters);
+    ERL_NIF_TERM headers = wrap_kvpairs(env, payload.headers);
+    ERL_NIF_TERM body = wrap_string(env, payload.name);
+    ERL_NIF_TERM schema = wrap_string(env, payload.name);
+    return enif_make_tuple7(env, a_payload, name, description, parameters, headers, body, schema);
+}
+
+static ERL_NIF_TERM wrap_resource(ErlNifEnv* env, const Resource resource) {
+    ERL_NIF_TERM uri_template = wrap_string(env, resource.uriTemplate);
+    ERL_NIF_TERM name = wrap_string(env, resource.name);
+    ERL_NIF_TERM description = wrap_string(env, resource.description);
+    ERL_NIF_TERM model = wrap_payload(env, resource.model);
+    ERL_NIF_TERM parameters = wrap_parameters(env, resource.parameters);
+    ERL_NIF_TERM headers = wrap_kvpairs(env, resource.headers);
+    ERL_NIF_TERM actions = wrap_actions(env, resource.actions);
+    return enif_make_tuple8(env, a_resource, uri_template, name, description, model, parameters, headers, actions);
+}
+
+static ERL_NIF_TERM wrap_resources(ErlNifEnv* env, const Collection<Resource>::type resources) {
+    ERL_NIF_TERM list = enif_make_list(env, 0);
+    for (Collection<Resource>::const_iterator it = resources.begin(); it != resources.end(); ++it) {
+        list = enif_make_list_cell(env,wrap_resource(env,*it),list);
+    }
+    return list;
+}
+
+static ERL_NIF_TERM wrap_resourceGroup(ErlNifEnv* env, const ResourceGroup resourceGroup) {
+    ERL_NIF_TERM name = wrap_string(env, resourceGroup.name);
+    ERL_NIF_TERM description = wrap_string(env, resourceGroup.description);
+    return enif_make_tuple4(env, a_resource_group, name, description, wrap_resources(env, resourceGroup.resources));
+}
+
+static ERL_NIF_TERM wrap_resourceGroups(ErlNifEnv* env, const Collection<ResourceGroup>::type resourceGroups) {
+    ERL_NIF_TERM list = enif_make_list(env, 0);
+    for (Collection<ResourceGroup>::const_iterator it = resourceGroups.begin(); it != resourceGroups.end(); ++it) {
+        list = enif_make_list_cell(env,wrap_resourceGroup(env,*it),list);
+    }
+    return list;
+}
+
+static ERL_NIF_TERM wrap_blueprint(ErlNifEnv* env, const Blueprint blueprint) {
+    ERL_NIF_TERM name = wrap_string(env, blueprint.name);
+    ERL_NIF_TERM description = wrap_string(env, blueprint.description);
+    return enif_make_tuple5(env, a_blueprint, wrap_kvpairs(env, blueprint.metadata), name, description, wrap_resourceGroups(env, blueprint.resourceGroups));
 }
 
 static ERL_NIF_TERM parse_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
@@ -108,11 +201,11 @@ static ERL_NIF_TERM parse_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[
             if (enif_compare(a_json, argv[0]) == 0) {
                 std::stringstream ss;
                 SerializeJSON(blueprint, ss);
-                term = enif_make_string(env, ss.str().c_str(), ERL_NIF_LATIN1);
+                term = wrap_string(env, ss.str());
             } else if (enif_compare(a_yaml, argv[0]) == 0) {
                 std::stringstream ss;
                 SerializeYAML(blueprint, ss);
-                term = enif_make_string(env, ss.str().c_str(), ERL_NIF_LATIN1);
+                term = wrap_string(env, ss.str());
             } else {
                 term = wrap_blueprint(env, blueprint);
             }
--- include/raven.hrl
+++ include/raven.hrl
@@ -9,3 +9,35 @@
     error,
     warnings
 }).
+
+-record(payload, {
+    name,
+    description,
+    parameters,
+    headers,
+    body,
+    schema
+}).
+
+-record(resource, {
+    uri_template,
+    name,
+    description,
+    model,
+    parameters,
+    headers,
+    actions
+}).
+
+-record(resource_group, {
+    name,
+    description,
+    resources
+}).
+
+-record(blueprint, {
+    metadata,
+    name,
+    description,
+    resource_groups
+}).
